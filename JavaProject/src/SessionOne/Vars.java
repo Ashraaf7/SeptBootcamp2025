bfc4ee746a789289ba782ea8aab56b7f68d61b70
Ashraaf7 <ahmedashraaf09@gmail.com>
2026-01-02 19:48:38 +0200

    Session 1 Code

---
+++ JavaProject/src/SessionOne/Vars.java
@@ -0,0 +1,36 @@
+package SessionOne;
+
+public class Vars {
+
+    public static void main(String[] args) {
+        //Variables > Container contains any value
+        // Data Types
+        double price = 10.5; // = assignment operator
+        int x = 256; // = assignment operator
+        boolean inStock = true;
+        char currency = '$';
+        String productName = "Back bag";
+
+        System.out.println("********** Product Information **********");
+        System.out.println("Product Name: " + productName);
+        System.out.println("Price: " + price + currency);
+        System.out.println("Product is in stock: " + inStock);
+    }
+
+    /*
+    * Fixed Number
+    byte > 8-bits
+    short > 16 bit
+    int >32 bit *****
+    long > 64 bit (L)
+    * Fractions
+    float > 32 bit (F)
+    double  > 64 bit ****
+    * True/False
+    boolean
+    * Single char
+    char
+    * Multi-Chars
+    String
+     */
+}
---
+++ SampleTest/HelloJava2026.java
@@ -0,0 +1,5 @@
+public class HelloJava2026 {
+    public static void main(String[] args) {
+        System.out.println("Hello Java 2026");
+    }
+}
